e25d7239074faac2c06f5fdc91883555e30565ed
wansimathurin <wansimath123@gmail.com>
2025-06-22 09:53:53 +0100

    contact form added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,77 @@
+import Navbar from "@/components/navbarComponent/Navbar";
 import { FaHeart } from "react-icons/fa";
 
 const page = () => {
     return (
-        <div className="bg-amber-600 h-screen flex items-center justify-center flex-col p-5">
-            <div className="flex overflow-hidden rounded-md w-[350px] h-[500px] relative">
-                <img src="/tenor.jpg" alt="" width={"100%"} height={100} />
-                <FaHeart size={25} color={"#fff"} className="absolute top-5  right-4 cursor-pointer" />
-            </div>
+        <>
+        <Navbar />
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+          {/* <ToastContainer
+            position="top-center"
+          />  */}
+      <div className="bg-white rounded-xl shadow-lg overflow-hidden flex flex-col md:flex-row w-full max-w-5xl">
+        
+        {/* Left Side Image */}
+        <div className="md:w-1/2 h-64 md:h-auto">
+          <img
+            src="https://platform.polygon.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/24458108/captain_pikachu.jpg?quality=90&strip=all&crop=7.8125,0,84.375,100"
+            alt="Contact"
+            className="w-full h-full object-cover"
+          />
         </div>
+
+        {/* Right Side Form */}
+        <div className="md:w-1/2 p-8">
+          <h2 className="text-3xl font-bold text-gray-800 mb-6">Contact Us</h2>
+          <div className="space-y-4">
+            <div>
+              <label className="block text-gray-700">Name</label>
+              <input
+                type="text"
+                              placeholder="Your Name"
+                              name="name"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Subject</label>
+              <input
+                type="text"
+                placeholder="Subject"
+                name="subject"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Message</label>
+              <textarea
+                rows="4"
+                placeholder="Your Message"
+                name="message"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              ></textarea>
+            </div>
+            <button
+               type="button"
+            //    onClick={()=>notify()}
+              className="w-full bg-[#F6C63E] text-white py-2 rounded-md hover:bg-amber-700 transition"
+            >
+              Send Message
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+       </>
     )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,16 @@
-'use client'
+"use client";
+import Navbar from "@/components/navbarComponent/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0)
-  const [textEntered,setTextEntered] = useState("")
-  //  let counter = 0
-   
   return (
-    <div className="bg-amber-600 h-screen flex items-center justify-center flex-col">
-      
-      <form >
-        <input value={textEntered} type="text" onChange={(e)=>setTextEntered(e.target.value)}  placeholder="enter a value..." className="bg-white px-3 py-3" />
-      </form>
-      <h1 className="text-white font-bold text-4xl">Entered Text : {textEntered} </h1>
-      
-      
-      <h1 className="text-white font-bold text-9xl">{counter} </h1>
-       <div className="flex gap-5">
-      <button onClick={()=>setCounter(counter+1)} className="bg-green-600 py-2 px-5 text-white mt-3"> + increment </button>
-      <button onClick={()=>counter > 0 && setCounter(counter-1)} className="bg-red-600 py-2 px-5 text-white mt-3"> - decrement</button>
-      </div>
-      <button onClick={()=>setCounter(0)} className="bg-red-600 py-2 px-5 text-white mt-3"> reset</button>
-       <Link href={"/contact"} className="bg-violet-800 py-2 px-5 text-white mt-3">Go to contact</Link>
+    <>
+    <Navbar />
+    <div className="bg-white h-screen flex items-center justify-center ">
+      <h1 className="text-amber-600">hom page</h1>
     </div>
+    </>
   );
 }
